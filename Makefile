a8867f5fdcab99af20b7bd84d71272024d68f860
Brenda DAcunha <bdacunha@outlook.com>
2015-11-19 10:35:40 -0800

    commit

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ clean:
 
 words.txt: /usr/share/dict/words
 	cp /usr/share/dict/words words.txt
+
+histogram.tsv: histogram.r words.txt
+	Rscript histogram.r
+	
---
+++ histogram.r
@@ -0,0 +1,4 @@
+words <- readLines('words.txt')
+word_length <- nchar(words)
+freq_table <- as.data.frame(table(word_length))
+write.table(freq_table, 'histogram.tsv', quote = FALSE, sep = '\t', row.names = FALSE)
